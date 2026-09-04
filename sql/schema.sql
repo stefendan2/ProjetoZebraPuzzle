805ac6b3b2c397abee5cd132c1e41d5affde95d7
stefendan <danilo.stefen@aluno.ifsp.edu.br>
2026-09-04 11:49:23 -0300

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -164,9 +164,9 @@ CREATE TABLE IF NOT EXISTS verificacao_email (
     INDEX idx_verificacao_email_jogador (jogador_id),
     INDEX idx_verificacao_email_expiracao (expira_em)
 ) ENGINE=InnoDB;
-
+/*
 CREATE USER IF NOT EXISTS 'zebrapuzzle_app'@'127.0.0.1'
-IDENTIFIED BY '123456';
+IDENTIFIED BY '';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON zebraPuzzle.*
@@ -181,3 +181,4 @@ FROM mysql.time_zone_name;
 SELECT *
 FROM mysql.time_zone_name
 WHERE Name = 'America/Sao_Paulo';
+*/
